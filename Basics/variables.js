4d2f9b156e43af989b02c0f34202eeb2e4543cd8
musawerxd <hunnyshah0012@gmail.com>
2026-01-29 14:27:12 +0500

    basics/variables

---
+++ Basics/variables.js
@@ -0,0 +1,33 @@
+
+
+// JS Variables Notes 1/29/2026
+"use strict";
+// Use "use strict" to avoid accidental globals
+//e.g:
+// anything = 5; // This will throw an error in "use strict"; mode other than that it will create a global variable.
+
+// Constants: never change
+const accountPassword = "mypassword123";
+
+// Let: can change, block scoped
+let accountEmail = "mygmail@gmail.com";
+let accountState; // empty for now
+let accountCity = "Burewala"; // proper declaration
+
+// Var: old-school, function scoped, avoid if possible
+var accountName = "Musawer";
+
+// Updating stuff
+accountEmail = "hc@hc.com";
+accountCity = "Bengaluru";
+
+// Logging
+console.log(accountName);
+console.table([accountName, accountEmail, accountPassword, accountCity]);
+// It displays data in a tabular format.
+
+/* Notes:
+- Prefer let/const, avoid var
+- const = never change, let = can change
+- No accidental globals!
+*/
---
+++ README.md
@@ -0,0 +1,8 @@
+# My JavaScript Notes 📝
+
+Just started learning JavaScript, posting **code snippets and notes** here.  
+Messy, experimental, and full of learning moments 😅  
+
+Feel free to **check my code, point out mistakes**, or drop tips!  
+
+Made with chai☕ and curiosity
